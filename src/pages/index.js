33814231809d656a623f31c42f8922456a4a976a
Delta Mike <87529812+DELTAMIKE7@users.noreply.github.com>
2024-07-18 03:41:32 +0530

    ui ready landing page

---
+++ src/pages/index.js
@@ -0,0 +1,15 @@
+import Image from 'next/image';
+import { Inter } from 'next/font/google';
+import LandingPage from './home/LandingPage';
+
+const inter = Inter({ subsets: ['latin'] });
+
+export default function Home() {
+	return (
+		<main
+			className={`flex min-h-screen flex-col items-center justify-between p-4 ${inter.className}`}
+		>
+			<LandingPage/>
+		</main>
+	);
+}
